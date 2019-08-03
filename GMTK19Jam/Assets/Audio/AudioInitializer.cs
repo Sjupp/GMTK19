f9dc1aaa87bf27e1d973608788aecf884cbbfcc3
Christopher <chrtaq1027@skola.goteborg.se>
2019-08-03 20:15:10 +0200

    Added Audio

---
+++ GMTK19Jam/Assets/Audio/AudioInitializer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioInitializer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ServiceLocator.Initialize();
+        ServiceLocator.ProvideAudio(new NewAudioProvider());
+        ServiceLocator.GetAudio().LoadSounds();
+        ServiceLocator.GetAudio().PlaySound("Music_Gameplay01");
+        ServiceLocator.GetAudio().PlaySound("VO_ReadyGo");
+    }
+
+    // For testing
+    //void Update()
+    //{
+    //    if(Input.GetKeyDown(KeyCode.Space))
+    //    {
+    //        ServiceLocator.GetAudio().PlaySound("UI_Select");
+    //    }
+    //}
+}
