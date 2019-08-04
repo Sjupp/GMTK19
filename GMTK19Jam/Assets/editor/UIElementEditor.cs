77a597547e50f05ff9b05c3428cb2994e03c1387
Simon Ektjärn <debarquer@gmail.com>
2019-08-04 12:39:28 +0200

    Added custom editor for UIElements

---
+++ GMTK19Jam/Assets/editor/UIElementEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(UIElement))]
+[CanEditMultipleObjects]
+public class UIElementEditor : Editor
+{
+    SerializedProperty interfaceState;
+
+    private void OnEnable() {
+        interfaceState = serializedObject.FindProperty("interfaceStates");
+    }
+
+    public override void OnInspectorGUI() {
+
+
+        serializedObject.Update();
+
+
+        EditorGUILayout.PropertyField(interfaceState, true);
+
+        serializedObject.ApplyModifiedProperties();
+
+    }
+}
